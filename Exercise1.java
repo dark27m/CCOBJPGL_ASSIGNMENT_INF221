4773707f2eff6b2e57c6caadb1614ef1af4dad26
Lance Carangan <115694658+dark27m@users.noreply.github.com>
2023-04-20 11:00:30 +0800

    Update Exercise1.java

--- Exercise1.java
+++ Exercise1.java
@@ -3,14 +3,14 @@ public class App {
     static boolean iwillNotCheat = true;
     static boolean IwillWin = true;
     public static void main(String[] args) throws Exception {
+        
+        enterCompetition();
         assert iwillNotCheat == true: "Sorry my bad, I didn't mean to do it.";
 
-        enterCompetition();
-
         assert IwillWin == true: "NOOO!";
+    
     }
-
     static void enterCompetition(){
-        IwillWin = false;
+        IwillWin= false;
     }
 }
